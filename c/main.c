5386da4fad0f1dfb285fa112891c0c71ff596256
Dezmou <incredibledez@gmail.com>
2022-10-08 15:02:11 +0200

    wip

--- c/main.c
+++ c/main.c
@@ -1,3 +1,5 @@
+// #include <stdlib.h>
+
 typedef struct  data {
     int time;
     float open;
@@ -7,6 +9,27 @@ typedef struct  data {
     float volume;
 } Data;
 
-void getSituation(Data *data, int *res, int cursor) {
-    res[0] = 55;
+typedef struct  result {
+    int close;
+} Result;
+
+typedef struct  misc {
+    int max;
+    int min;
+} Misc;
+
+void getSituation(Data *data, Result *res, Misc * misc, int cursor, int situationSize) {
+    int max = 0;
+    int min = 999999;
+    for (int i=0; i< situationSize; i++){
+        res[i].close =  (int)((data[cursor + i].close / data[cursor + i + 1].close * 10000));
+        if (res[i].close < min) {
+            min = res[i].close;
+        }
+        if (res[i].close > max) {
+            max = res[i].close;
+        }
+        misc->min = min;
+        misc->max = max;
+    }
 }
--- trade/src/App.tsx
+++ trade/src/App.tsx
@@ -2,11 +2,23 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 
 const LINE_SIZE = 4 * 6;
-const SITUATION_NBR = 4;
 const SITUATION_SIZE = 100;
+const SITUATION_AMOUNT = 4;
+const SITUATION_STEP = 5;
 
 interface Abi {
-  getSituation: (memData: number, memRresult: number, cursor: number) => void
+  getSituation: (
+    memData: number,
+    memRresult: number,
+    misc: number,
+    cursor: number,
+    situationSize: number
+  ) => void
+}
+
+const misc = {
+  resMin: 0,
+  resMax: 0,
 }
 
 function App() {
@@ -16,15 +28,17 @@ function App() {
 
       // Set memory
       const memory = new WebAssembly.Memory({
-        initial: (data.byteLength / 64000) + (LINE_SIZE * SITUATION_SIZE * SITUATION_NBR)
+        initial: (data.byteLength / 64000) + (SITUATION_SIZE * 4) + (Object.keys(misc).length * 4)
       });
       (new Uint8ClampedArray(memory.buffer, 0, data.byteLength))
         .set(new Uint8ClampedArray(data, 0, data.byteLength), 0)
 
       const ptr_data = 0;
       const ptr_result = data.byteLength;
+      const prt_misc = data.byteLength + (SITUATION_SIZE * 4);
 
-      const result = new Uint32Array(memory.buffer, data.byteLength);
+      const situationResult = new Int32Array(memory.buffer, data.byteLength);
+      const miscResult = new Int32Array(memory.buffer, data.byteLength + (SITUATION_SIZE * 4));
 
       // Load asm
       const loadAsm = async (scriptSrc: string) => {
@@ -38,11 +52,11 @@ function App() {
         return (instance.exports as any) as Abi
       }
       const funcs = await loadAsm("./main.wasm")
-
-      console.log(result.at(0));
-      const res = funcs.getSituation(0, ptr_result, 5000)
-      console.log(result.at(0));
-
+      for (let i = 0; i < 100; i++) {
+        const cursor = 10000 + i * 100;
+        funcs.getSituation(0, ptr_result, prt_misc, cursor, SITUATION_SIZE);
+        console.log(miscResult.at(1));
+      }
     })()
   }, [])
 
